fc1e35859405dbe68f3e4cc7bd891d1d2192c99e
Alex Huk <alex.huk85@gmail.com>
2021-09-21 09:19:03 +1000

    Email server done

--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+from django import forms
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
+from django.conf import settings
+from phonenumber_field.formfields import PhoneNumberField
 
-# Create your models here.
+class ContactForm(forms.Form):
+    yourname = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(required=False, label='Your Email Address')
+    phone_number = PhoneNumberField(label='Your Phone Number')
+    message = forms.CharField(widget=forms.Textarea)
+
+def contact(request):
+    submitted = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            email_from = settings.EMAIL_HOST_USER
+            email_to = cd.get('email', 'siteowner@example.com')
+            #assert False
+            #con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                email_from,
+                [email_to, 'alex.huk85@gmail.com']
+                #connection=con
+            )
+            return HttpResponseRedirect('/contact?submitted=True')
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request,
+        'contact/contact.html',
+        {'form': form, 'submitted': submitted})
--- main/views.py
+++ main/views.py
@@ -1,5 +1,44 @@
+from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
+from django.conf import settings
 
+
+
+class ContactForm(forms.Form):
+    yourname = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(required=False, label='Your Email Address')
+    phone_number = forms.CharField(max_length=20, label='Your Phone Number')
+    message = forms.CharField(widget=forms.Textarea)
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    submitted = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            sender = cd['yourname'] +' '+ cd['phone_number']
+            email_from = settings.EMAIL_HOST_USER
+            email_to = cd.get('email', 'siteowner@example.com')
+            #assert False
+            #con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(
+                sender,
+                cd['message'],
+                email_from,
+                [email_to, 'taxhelpngrowth@gmail.com']
+                #connection=con
+            )
+            return HttpResponseRedirect('/?submitted=True')
+
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
+            #return HttpResponseRedirect('/')
+    
+
+    return render(request,
+        'main/index.html',
+        {'form': form, 'submitted': submitted})
